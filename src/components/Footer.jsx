0ee6de6a536369250dd1f0ff3a3994a498b4b4ec
Abhinav Mehta <abhinavmehta257@gmail.com>
2026-03-18 01:08:15 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className='w-full flex justify-center py-6 mt-[72px] border-t border-[#C9B0C8]'>
         <p className='text-[14px] font-inter text-[#999999]'>
-            © 2025 Anushka Mukherjee
+            © 2026 Anushka Mukherjee
         </p>
     </footer>
 )
